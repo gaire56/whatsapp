2a03f8d97ea16e292e7cbc4b83ef185f39477fef
Anant Gaire <56.anant@gmail.com>
2021-03-28 11:22:00 +0545

    firebase

---
+++ components/Sidebar.js
@@ -0,0 +1,79 @@
+import { Avatar, Button, IconButton } from '@material-ui/core';
+
+import styled from 'styled-components';
+import ChatIcon from '@material-ui/icons/Chat';
+import SearchIcon from '@material-ui/icons/Search';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+function Sidebar() {
+  return (
+    <Container>
+      <Header>
+        <UserAvatar />
+        <IconsContainer>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </IconsContainer>
+      </Header>
+
+      <Search>
+        <SearchIcon />
+        <SearchInput placeholder="search in chats" />
+      </Search>
+      <SidebarButton>Start a new chat</SidebarButton>
+    </Container>
+  );
+}
+
+export default Sidebar;
+
+const Container = styled.div``;
+
+const Search = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  border-radius: 2px;
+`;
+
+const SidebarButton = styled(Button)`
+  width: 100%;
+
+  &&& {
+    border-top: 1px solid whitesmoke;
+    border-bottom: 1px solid whitesmoke;
+  }
+`;
+
+const SearchInput = styled.input`
+  outline-width: 0;
+  border: none;
+  flex: 1;
+`;
+
+const Header = styled.div`
+  display: flex;
+  position: sticky;
+  top: 0;
+  background-color: white;
+  z-index: 1;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  height: 80px;
+  border-bottom: 1px solid whitesmoke;
+`;
+
+const UserAvatar = styled(Avatar)`
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.8;
+  }
+`;
+
+const IconsContainer = styled.div``;
